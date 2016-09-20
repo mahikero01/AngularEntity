98b3bd0c55af9e3f75b8cd1f6095ca4d345c3192
mahikero <federicoparassarmiento@yahoo.com>
2016-09-20 15:21:11 -0400

    ini

---
+++ Solution/AE_BusinessLayer/LanguageBL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AE_DataAccessLayer;
+using AE_Web;
+
+namespace AE_BusinessLayer
+{
+    public class LanguageBL
+    {
+        private LanguageDAL DAL;
+
+        public LanguageBL()
+        {
+            this.DAL = new LanguageDAL();
+        }
+
+        public List<AE_Languages> GetAllLanguages()
+        {
+            var data = DAL.ReadLanguages();
+
+            List<AE_Languages> languageList = new List<AE_Languages>();
+
+            foreach (AE_Languages language in data)
+            {
+                languageList.Add(language);
+            }
+
+            return languageList;
+        }
+    }
+}
---
+++ Solution/AE_Web/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using AE_BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace AE_Web
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LanguageBL test = new LanguageBL();
+            var languages = test.GetAllLanguages();
+            var jsondata = JsonConvert.SerializeObject(languages);
+            
+        }
+
+        
+    }
+}
